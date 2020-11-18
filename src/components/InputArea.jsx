8758b2b2bed881d9b1f0e91ae123146d55f30e71
Olaoye Victor <52793798+OlaoyeVic@users.noreply.github.com>
2020-11-18 03:27:22 +0100

    A to-do list app with functional components in React

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputArea() {
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
+      <button
+        onClick={() => {
+          props.onAdd(inputText);
+          setInputText("");
+        }}
+      >
         <span>Add</span>
       </button>
     </div>
